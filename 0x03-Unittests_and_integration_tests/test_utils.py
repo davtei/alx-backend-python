8f62b171c4c1069ec2c653200b9eb06119528f04
David Tei <daveteidt@gmail.com>
2023-12-01 21:18:33 +0930

    Refactor test_access_nested_map_error to test_access_nested_map_exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,7 +29,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
     ])
-    def test_access_nested_map_error(
+    def test_access_nested_map_exception(
             self,
             nested_map: Dict,
             path: Tuple[str],
