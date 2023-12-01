3432f7b96d54626fc9147020af55bea4104ea0d7
David Tei <daveteidt@gmail.com>
2023-12-01 22:03:46 +0930

    Add unit test for GithubOrgClient.org method

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""A github org client test suite"""
+
+import unittest
+from typing import Dict
+from unittest.mock import MagicMock, Mock, PropertyMock, patch
+
+from client import GithubOrgClient
+from parametized import parametized, parametized_class
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """A GithubOrgClient test suite"""
+
+    @parametized.expand({
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    })
+    @patch(
+        "client.get_json",
+    )
+    def test_org(self, org: str, expected: Dict, mock_get_json: MagicMock
+                 ) -> None:
+        """Test GithubOrgClient.org"""
+        mock_get_json.return_value = MagicMock(return_value=expected)
+        gh_org_client = GithubOrgClient(org)
+        self.assertEqual(gh_org_client.org(), expected)
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
